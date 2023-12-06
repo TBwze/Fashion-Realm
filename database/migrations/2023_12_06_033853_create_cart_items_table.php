758b77dcfcc5ad711ef2dedde62342c33a65bc6b
TBwze <darreniskandar27@gmail.com>
2023-12-06 19:34:36 +0700

    model, migration, seeder

--- database/migrations/2023_12_06_033853_create_cart_items_table.php
+++ database/migrations/2023_12_06_033853_create_cart_items_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->unsignedBigInteger('product_size_id');
+            $table->int('quantity');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cart_items');
     }
 };
