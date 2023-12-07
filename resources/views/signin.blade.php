89a7508c6314e1191bfd7bf0415d2d6a743b7ae5
Darren Iskandar Cahyadi <darreniskandar27@gmail.com>
2023-12-08 01:30:17 +0700

    signin

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -1,8 +1,9 @@
 @extends('layout')
 
-@section('contents')
+@section('content')
+    <link rel="stylesheet" href="css/signin_style.css" />
     <div class="container" id="container">
-        <div class="form-container. sign-up">
+        <div class="form-container sign-up">
             <form action="/signup" method="post" enctype="multipart/form-data">
                 {{ csrf_field() }}
                 <h1>Sign Up</h1>
@@ -15,17 +16,17 @@
                 <input type="email" name="email" placeholder="Email">
                 <input type="password" name="password" placeholder="Password">
                 <input type="password" name="confirmpassword" placeholder="Confirm Password">
-                @if ($errors->any())
+                {{-- @if ($errors->any())
                     <ul>
                         @foreach ($errors->all() as $error)
                             <li style="color: red">{{ $error }}</li>
                         @endforeach
                     </ul>
-                @endif
+                @endif --}}
                 <button>Sign Up</button>
             </form>
         </div>
-        <div class="form-container. sign-in">
+        <div class="form-container sign-in">
             <form action="/signin" method="post" enctype="multipart/form-data">
                 {{ csrf_field() }}
                 <h1>Sign In</h1>
@@ -37,25 +38,18 @@
                 <input type="email" placeholder="Email">
                 <input type="password" placeholder="Password">
                 <a href="#">Forgot Your Password?</a>
-                @if ($errors->any())
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li style="color: red">{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @endif
                 <button>Sign In</button>
             </form>
         </div>
         <div class="toggle-container">
             <div class="toggle">
-                <div class="toggle-panel. toggle-left">
+                <div class="toggle-panel toggle-left">
                     <h1>Welcome Back!</h1>
                     <p>Start shopping with us today</p>
                     <button class="hidden" id="login">Sign In</button>
                 </div>
-                <div class="toggle-panel. toggle-right">
-                    <h1>Welcome dear customers</h1>
+                <div class="toggle-panel toggle-right">
+                    <h1>Welcome Dear Customers!</h1>
                     <p>Create an account today</p>
                     <button class="hidden" id="register">Sign Up</button>
                 </div>
@@ -63,5 +57,5 @@
         </div>
     </div>
 
-    <script src="js/script.js"></script>
+    <script src="js/signin_script.js"></script>
 @endsection
