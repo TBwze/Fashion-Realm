b1d1295d3d8e97074081e185b488a80abdd80cc1
Darren Iskandar Cahyadi <darreniskandar27@gmail.com>
2023-12-07 22:55:31 +0700

    layouut

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('contents')
+    <div>
+        <div>
+            <form>
+                <h1>Create Account</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google"></i></a>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
