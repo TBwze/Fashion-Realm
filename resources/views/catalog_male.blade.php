da1e42f77d84754173502bbee7c835b23fd93f2d
Bry <bryannicholasliu@gmail.com>
2023-12-09 11:38:10 +0700

    gridview-catalog-onprogress

--- resources/views/catalog_male.blade.php
+++ resources/views/catalog_male.blade.php
@@ -13,6 +13,7 @@
       </div>
     </div>
 
+    <!-- dropdown for sorting -->
     <div class="dropdown">
       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       Release Date
@@ -21,6 +22,18 @@
         <a class="dropdown-item" href="#">Sort by Oldest</a>
         <a class="dropdown-item" href="#">Sort by Latest</a>
         <a class="dropdown-item" href="#">Sort by Best Seller</a>
+      </div>
+    </div>
+    
+    <!-- cloth gridview (ON PROGRESS) -->
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="..."> <!-- clothing img -->
+      <div class="card-body">
+        <!-- clothing name -->
+        <h5 class="card-title">Clothing name</h5>
+
+        <!-- price -->
+        <p class="card-text">Rp. 500.000,00</p>
       </div>
     </div>
 
