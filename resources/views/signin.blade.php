b8253cf143923580a22976a8e051bbd7ce129e85
Darren Iskandar Cahyadi <darreniskandar27@gmail.com>
2023-12-09 21:41:28 +0700

    signin, ada ganti layout dikit

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -1,61 +1,35 @@
 @extends('layout')
 
 @section('content')
-    <link rel="stylesheet" href="css/signin_style.css" />
-    <div class="container" id="container">
-        <div class="form-container sign-up">
-            <form action="/signup" method="post" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                <h1>Sign Up</h1>
-                <div class="social-icons">
-                    <a href="#" class="icon"><i class="fa-brands fa-google"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-facebook"></i></i></a>
+    <link rel="stylesheet" href="css/signin_style.css">
+    <div class="wrapper">
+        <div class="container main">
+            <div class="row">
+                <div class="col-md-6 side-image">
+                    <div class="text">
+                        <p>Start shopping with us today!</p>
+                    </div>
                 </div>
-                <span>or create a new account</span>
-                <input type="text" name="name" placeholder="Name">
-                <input type="email" name="email" placeholder="Email">
-                <input type="password" name="password" placeholder="Password">
-                <input type="password" name="confirmpassword" placeholder="Confirm Password">
-                {{-- @if ($errors->any())
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li style="color: red">{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @endif --}}
-                <button>Sign Up</button>
-            </form>
-        </div>
-        <div class="form-container sign-in">
-            <form action="/signin" method="post" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                <h1>Sign In</h1>
-                <div class="social-icons">
-                    <a href="#" class="icon"><i class="fa-brands fa-google"></i></a>
-                    <a href="#" class="icon"><i class="fa-brands fa-facebook"></i></i></a>
-                </div>
-                <span>or use a pre-existing account</span>
-                <input type="email" placeholder="Email">
-                <input type="password" placeholder="Password">
-                <a href="#">Forgot Your Password?</a>
-                <button>Sign In</button>
-            </form>
-        </div>
-        <div class="toggle-container">
-            <div class="toggle">
-                <div class="toggle-panel toggle-left">
-                    <h1>Welcome Back!</h1>
-                    <p>Start shopping with us today</p>
-                    <button class="hidden" id="login">Sign In</button>
-                </div>
-                <div class="toggle-panel toggle-right">
-                    <h1>Welcome Dear Customers!</h1>
-                    <p>Create an account today</p>
-                    <button class="hidden" id="register">Sign Up</button>
+                <div class="col-md-6 right">
+                    <div class="input-box">
+                        <header>Sign in</header>
+                        <div class="input-field">
+                            <input type="text" class="input" id="email" required="" autocomplete="off">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="input-field">
+                            <input type="password" class="input" id="pass" required="">
+                            <label for="pass">Password</label>
+                        </div>
+                        <div class="input-field">
+                            <input type="submit" class="submit" value="Sign In">
+                        </div>
+                        <div class="signin">
+                            <span>Don't have an account yet? <a href="#">Create here</a></span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <script src="js/signin_script.js"></script>
 @endsection
