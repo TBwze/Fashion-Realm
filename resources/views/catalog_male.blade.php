e3e9b4618f82d973075963c071841459cc08b46c
Bry <bryannicholasliu@gmail.com>
2023-12-11 11:51:11 +0700

    gridview-catalogmen

--- resources/views/catalog_male.blade.php
+++ resources/views/catalog_male.blade.php
@@ -1,8 +1,10 @@
     @extends('layout')
 
-    @section('title', 'Catalog - Men')
+    @section('title', 'Fashion Realm - Catalog (Men)')
 
     @section('content')
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v6.5.1/css/all.css">
 
     <div class="text-center fst-italic pt-4">
       <h7>Fashion Realm / Collections</h7>
@@ -13,7 +15,7 @@
     </div>
 
     <!-- dropdown for sorting -->
-    <div class="dropdown">
+    <div class="dropdown show px-5">
       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       Release Date
       </button>
@@ -25,18 +27,147 @@
     </div>
 
     <br>
-    
-    <!-- cloth gridview (ON PROGRESS) -->
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="..."> <!-- clothing img -->
-      <div class="card-body">
-        <!-- clothing name -->
-        <h5 class="card-title">Clothing name</h5>
 
-        <!-- price -->
-        <p class="card-text">Rp. 500.000,00</p>
-      </div>
-    </div>
+<div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/oversized-blue-tee.png') }}" data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">cloth name</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    </div>
+                    <small class="text-muted">price</small>
+                  </div>
+                </div>
+              </div>
+            </div>
 
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/oversized-blue-tee.png') }}" data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">cloth name</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    </div>
+                    <small class="text-muted">price</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/oversized-blue-tee.png') }}" data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">cloth name</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    </div>
+                    <small class="text-muted">price</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/oversized-blue-tee.png') }}" data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">cloth name</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    </div>
+                    <small class="text-muted">price</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/oversized-blue-tee.png') }}" data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">cloth name</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    </div>
+                    <small class="text-muted">price</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/oversized-blue-tee.png') }}" data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">cloth name</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    </div>
+                    <small class="text-muted">price</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/oversized-blue-tee.png') }}" data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">cloth name</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    </div>
+                    <small class="text-muted">price</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/oversized-blue-tee.png') }}" data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">cloth name</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    </div>
+                    <small class="text-muted">price</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/oversized-blue-tee.png') }}" data-holder-rendered="true">
+                <div class="card-body">
+                  <p class="card-text">cloth name</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    </div>
+                    <small class="text-muted">price</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+    <br>
     
     @endsection
